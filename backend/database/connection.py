0bbaddb64805ec284923660bfed6b7ebe14bd68c
lemessaA <lemessaahmed34@gmail.com>
2026-03-09 22:33:22 +0300

    database connecton and session

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -4,9 +4,9 @@ from sqlalchemy.orm import sessionmaker
 from config import settings
 
 engine = create_engine(settings.database_url)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # session factory its used for creating database sessions
 
-Base = declarative_base()
+Base = declarative_base() # base class for all models that means all models will inherit from this class
 
 
 async def init_db():
